867a9b6fe1bf2d730607050b0400e3ca952d55d1
Kathleen Mattos <kathleenmmattos@gmail.com>
2019-01-23 10:28:43 -0700

    added 2 ";"

--- mysql/oml.sql
+++ mysql/oml.sql
@@ -6,8 +6,9 @@ INSERT INTO user(userId, userActivationToken, userName, userPassword, userEmail)
 
 UPDATE user SET userName = "logan", userPassword = "phoenix" WHERE userId = UNHEX("cbcd4dc25b764827832f39650902384a");
 
-DELETE FROM user WHERE userId = UNHEX("b1eb3f9cd64a4bbab226f8ad69e04c13")
+DELETE FROM user WHERE userId = UNHEX("b1eb3f9cd64a4bbab226f8ad69e04c13");
 
-INSERT INTO post(postId, postUserId, postTitle, postContent, postUploadDate) VALUES (UNHEX("ff91b22b98be4cb2947011d6f0656073"), UNHEX("cbcd4dc25b764827832f39650902384a"), "Check out this article about adamantium.", "Here is a link to the article.", NOW())
+INSERT INTO post(postId, postUserId, postTitle, postContent, postUploadDate) VALUES (UNHEX("ff91b22b98be4cb2947011d6f0656073"), UNHEX("cbcd4dc25b764827832f39650902384a"), "Check out this article about adamantium.", "Here is a link to the article.", NOW());
 
 SELECT userId, userActivationToken, userName, userPassword, userEmail from user WHERE userId = UNHEX("c69bdfe79bb04b0cb8ef4c76041980fb");
+
